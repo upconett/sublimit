ae6454d7c32cf573a75abb4badfbd729e9d247fb
upconett <upconett@gmail.com>
2024-05-07 15:53:03 +0600

    Edited user.views
    Added User checking, login/register rendering

--- sublimit/main/views.py
+++ sublimit/main/views.py
@@ -4,13 +4,23 @@ from django.shortcuts import render, redirect
 from utility.functions import *
 from utility.exceptions import *
 
+from user.models import User, Article
+
 
 def redirector(request: HttpRequest):
     if request.method == "GET":
         try:
             email, username, password = get_auth_cookies(request)
-        except InvalidCookie:
-            return redirect('/login/')
+            User.objects.get(
+                email=email, 
+                username=username, 
+                password=password
+                )
+            return redirect('/home/')
+
+        except InvalidCookie: return redirect('/login/')
+        except User.DoesNotExist: return redirect('/login/')
+
     else:
         return Http404()
 
@@ -18,23 +28,28 @@ def redirector(request: HttpRequest):
 def home(request: HttpRequest):
     if request.method == "GET":
         pass
+
     else:
         return Http404()
 
 
 def login(request: HttpRequest):
     if request.method == "GET":
-        pass
+        return render(request, 'auth/login.html')
+
     elif request.method == "POST":
         pass
+
     else:
         return Http404()
 
 
 def register(request: HttpRequest):
     if request.method == "GET":
-        pass
+        return render(request, 'auth/register.html')
+
     elif request.method == "POST":
         pass
+
     else:
         return Http404()
