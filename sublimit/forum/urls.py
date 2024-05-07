77105ec2cb99314ba4cc2b4896a59786d398be39
upconett <upconett@gmail.com>
2024-05-07 11:34:39 +0600

    Added forum URLs

---
+++ sublimit/forum/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+
+from . import views
+
+regex = "(?P<article_id>\d+)"
+
+urlpatterns = [
+    path('', views.redirector),
+    path('article/new/', views.new_article),
+    re_path(f'article/{regex}', views.show_article),
+    re_path(f'article/{regex}/edit', views.edit_article),
+    re_path(f'article/{regex}/delete', views.delete_article),
+    re_path(f'article/{regex}/comment', views.comment_article),
+    re_path(f'article/{regex}/like', views.like_article),
+    re_path(f'article/{regex}/dislike', views.dislike_article),
+    re_path(f'article/{regex}/star', views.star_article)
+]
--- sublimit/forum/views.py
+++ sublimit/forum/views.py
@@ -1,3 +1,38 @@
+from django.http import *
 from django.shortcuts import render
 
-# Create your views here.
+
+def redirector(request: HttpRequest):
+    pass
+
+
+def new_article(request: HttpRequest):
+    pass
+
+
+def show_article(request: HttpRequest):
+    pass
+
+
+def edit_article(request: HttpRequest):
+    pass
+
+
+def delete_article(request: HttpRequest):
+    pass
+
+
+def comment_article(request: HttpRequest):
+    pass
+
+
+def like_article(request: HttpRequest):
+    pass
+
+
+def dislike_article(request: HttpRequest):
+    pass
+
+
+def star_article(request: HttpRequest):
+    pass
