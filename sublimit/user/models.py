d936283b03ff2d4f8c1302afe1b0289c7fe137d2
upconett <upconett@gmail.com>
2024-05-07 15:40:56 +0600

    Added user models
    Added User, Contact, Article

--- sublimit/user/models.py
+++ sublimit/user/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email: models.EmailField = models.EmailField()
+    username: models.CharField = models.CharField(max_length=20, unique=True)
+    password: models.CharField = models.CharField(max_length=35)
+    avatar: models.ImageField = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Contact(models.Model):
+    TELEGRAM = 'TG'
+    GITHUB = 'GH'
+    CONTACT_TYPE_CHOICES = {
+        TELEGRAM: "Telegram",
+        GITHUB: "GitHub"
+    }
+
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    type = models.CharField(
+        max_length=2,
+        choices=CONTACT_TYPE_CHOICES
+    )
+    link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.type}, {self.link}'
+
+
+class Article(models.Model):
+    author = models.ForeignKey("User", on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
+    text = models.TextField(max_length=10000)
