4ce7d6aad31600209f915ad59164cf3f2d8861cc
upconett <upconett@gmail.com>
2024-05-08 14:31:36 +0600

    Added user views and html
    Filling html TODO

--- sublimit/user/views.py
+++ sublimit/user/views.py
@@ -1,18 +1,69 @@
 from django.http import *
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from utility.functions import *
+from utility.exceptions import *
 
 
 def redirector(request: HttpRequest):
-    pass
+    if request.method == "GET":
+        try:
+            user = validate_user(request)
+            return redirect(f'/user/{user.username}/')
+
+        except InvalidCookie: return redirect('/login/')
+        except User.DoesNotExist: return redirect('/login/')
+
+    else:
+        return Http404()
 
 
-def show_user(request: HttpRequest):
-    pass
+def show_user(request: HttpRequest, username: str):
+    if request.method == "GET":
+        if username:
+            try:
+                user = User.objects.get(username=username)
+                return render(request, 'user/profile.html', {'user': user})
+            except User.DoesNotExist: return Http404()
+
+        else:
+            try:
+                user = validate_user(request)
+                return render(request, 'user/my_profile.html', {'user': user})
+
+            except InvalidCookie: return Http404()
+            except User.DoesNotExist: return Http404()
+
+    else:
+        return Http404()
 
 
-def follow_user(request: HttpRequest):
-    pass
+def follow_user(request: HttpRequest, username: str):
+    if request.method == "POST":
+        # try: me = validate_user(request)
+        # except InvalidCookie: return redirect('/login/')
+        # except User.DoesNotExist: return redirect('/login/')
+
+        # try: user = User.objects.get(username=username)
+        # except User.DoesNotExist: return redirect('')
+        pass
+    else:
+        return Http404()
 
 
 def settings(request: HttpRequest):
-    pass
+    if request.method == "GET":
+        try:
+            validate_user(request)
+            return render(request, 'user/settings.html')
+        
+        except InvalidCookie: return redirect('/login/')
+        except User.DoesNotExist: return redirect('/login/')
+    elif request.method == "POST":
+        # try:
+        #     validate_user(request)
+        # except InvalidCookie: return redirect('/login/')
+        # except User.DoesNotExist: return redirect('/login/')
+        pass
+    else:
+        return Http404()
